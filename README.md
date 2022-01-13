6c5202d2ed994f0973fcfd6087d5786c34e3e16a
hazlan.rozaimi <hazlan.rozaimi@fwd.com>
2022-01-13 12:32:49 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wordle Helper
+
+This helper is to help me do Wordle (https://www.powerlanguage.co.uk/wordle/)
+
+
+## Usage
+Basically just run it and replace `*` with the unknown alphabets
+
+Eg: If you got ?,?,B,U,? run the below command
+```
+node index.js "**bu*"
+```
+
+## Yellow words
+Lets not cheat too much (wink)
---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const query = process.argv[2].toLowerCase()
+
+if(query.length !== 5) {
+    console.error("query must be 5 letters")
+}
+else {
+    const data = fs.readFileSync('words_alpha.txt').toString();
+    const words = data.split('\n')
+        .map(word => word.replace('\r',''))
+        .filter(word => word.length === 5)
+
+    const regexStr = query.split('')
+        .map(alphabet => alphabet === '*'? '[a-z]': alphabet)
+        .join('')
+    const regex = new RegExp(regexStr);
+    const result = words.filter(word => regex.test(word))
+    console.log(result.join('\n'))
+}
